ee613b28827d366dbdaa553ffd6d7ba5992b8059
ikrai11 <hhnawws@gmail.com>
2026-09-05 13:22:21 +0800

    微服务拆分：抽取网页截图服务模块 screenshot

---
+++ ikrai-code-mother-microservice/ikrai-code-screenshot/src/main/java/com/aistd/ikraiaicodemother/IkraiCodeScreenshotApplication.java
@@ -0,0 +1,11 @@
+package com.aistd.ikraiaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkraiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IkraiCodeScreenshotApplication.class, args);
+    }
+}
