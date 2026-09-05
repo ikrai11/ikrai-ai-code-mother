7f266f20a7497dab9b9ded15b35eba90930d32d7
ikrai11 <hhnawws@gmail.com>
2026-09-05 12:30:23 +0800

    微服务拆分：抽取AI代码生成模块 ai

---
+++ ikrai-code-mother-microservice/ikrai-code-ai/src/main/java/com/aistd/ikraiaicodemother/ai/tool/ExitTool.java
@@ -0,0 +1,37 @@
+package com.aistd.ikraiaicodemother.ai.tool;
+
+import cn.hutool.json.JSONObject;
+import dev.langchain4j.agent.tool.Tool;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+/*@Component*///注释不使用这个工具，减少AI调用工具可能存在的稳定性
+public class ExitTool extends BaseTool {
+
+    @Override
+    public String getToolName() {
+        return "exit";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "退出工具调用";
+    }
+
+    /**
+     * 退出工具调用
+     * 当任务完成或无需继续使用工具时调用此方法
+     *
+     * @return 退出确认信息
+     */
+    @Tool("当任务已完成或无需继续调用工具时，使用此工具退出操作，防止循环")
+    public String exit() {
+        log.info("AI 请求退出工具调用");
+        return "不要继续调用工具，可以输出最终结果了";
+    }
+
+    @Override
+    public String generateToolExecutedResult(JSONObject arguments) {
+        return "\n\n[执行结束]\n\n";
+    }
+}
